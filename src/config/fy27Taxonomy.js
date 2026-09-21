83311ec7976519083fde9afcb5f80e92d0b6b95d
Bonnieeewxw <bonny_purpose@aliyun.com>
2026-09-21 13:17:03 +0800

    Add FY27 Event taxonomy

--- src/config/fy27Taxonomy.js
+++ src/config/fy27Taxonomy.js
@@ -41,3 +41,88 @@ export const marketingPlayOptions = {
   'Non-Solution Area Aligned': ['AI Opportunity', 'Business Applications', 'Compliant Ecosystem', 'Data and AI', 'Developer', 'Digital and App Innovation', 'Eval Center', 'Infrastructure', 'Marketplace Reach', 'Microsoft Research', 'Modern Work', 'Partner with MS', 'Secure Ecosystem', 'Software Development Company', 'Surface Devices', 'Windows OEM Devices', 'N/A'],
   'Cross Solution Area Aligned': ['Frontier Transformation', 'Education K12', 'Government', 'Healthcare Dragon Copilot'],
 };
+
+export const eventWorkstreamOptions = [
+  'Pre-Event',
+  'During Event',
+  'Post-Event',
+  'On the Ground',
+  'Creator',
+  'Activation',
+  'Other',
+];
+
+export const eventMktgOptions = [
+  'Flagship Events',
+  'Third Party Events',
+  'Digital Events',
+  'Not Aligned to an Event',
+];
+
+export const eventNameOptions = {
+  'Flagship Events': [
+    'Ignite',
+    'Build',
+    'AI Tour',
+  ],
+
+  'Third Party Events': [
+    'AHRA',
+    'Beckers',
+    'BETT',
+    'Black Hat',
+    'Cannes Lions',
+    'CERAWeek',
+    'Consumer Electronics Show',
+    'Envision',
+    'Epic UGM',
+    'Excel World Championships',
+    'Fabric Community Conference',
+    'FMX',
+    'Hannover Messe',
+    'HIMSS',
+    'HLTH',
+    'IBC',
+    'MGMA',
+    'Mobile World Congress',
+    'Money 2020',
+    'MPPC',
+    'MVP Summit',
+    'National Association of Broadcasters Show',
+    'NRF',
+    'Other Third-Party Event',
+    'RSAC',
+    'RSNA',
+    'SIBOS',
+    'SIIM',
+    'SmartCity',
+    'ViVE',
+    'Microsoft Digital Sovereignty Summit',
+    'FabCon',
+  ],
+
+  'Digital Events': [
+    'Ability Summit',
+    'Azure Migration Digital Event',
+    'Azure Open Day',
+    'Business Applications Launch Event',
+    'Global Scale Program',
+    'MCAPS Start for Partners',
+    'Microsoft Secure',
+    'MVTD',
+    'Copilot Virtual Training',
+    'SDC Virtual Training',
+    'M365 Conference',
+    'Cloud & AI Day',
+    'Fabric Roadshow',
+    'Security Summit',
+    'AI & Security Summit',
+    'Migrate & Modernize Summit',
+    'Sovereignty Day',
+    'Innovate with AI Agents',
+    'Windows Server Summit',
+    'Frontier Transformation Week',
+  ],
+
+  'Not Aligned to an Event': [],
+};
